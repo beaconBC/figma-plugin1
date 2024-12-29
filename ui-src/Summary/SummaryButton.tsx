d0e88b9f650f5f3c8d0c8cb0a53917319e8f6d69
beaconBC <abhbtbb1@gmail.com>
2024-12-29 17:04:44 +0900

    feat: Reset summary data after creation summary

--- ui-src/Summary/SummaryButton.tsx
+++ ui-src/Summary/SummaryButton.tsx
@@ -2,16 +2,25 @@ import { useContext } from "react";
 import { SummaryContext } from "./Summary";
 import { convertRequestedData } from "./Summary.type";
 import { SummaryButtonProps } from "./SummaryArea";
+import { produce } from "immer";
 
-export function SummaryButton({ className }: SummaryButtonProps) {
-  const { state } = useContext(SummaryContext);
+export default function SummaryButton({ className }: SummaryButtonProps) {
+  const { state, setState } = useContext(SummaryContext);
 
-  const onBox1 = () => {
+  const clickHandler = () => {
     const summaryData = convertRequestedData(state);
 
     parent.postMessage(
       { pluginMessage: { type: "create-summary", summaryData } },
       "*"
+    );
+
+    setState(
+      produce((draft) => {
+        draft.title.value = "";
+        draft.summary.value = "";
+        draft.description.value = "";
+      })
     );
   };
 
@@ -20,7 +29,7 @@ export function SummaryButton({ className }: SummaryButtonProps) {
       <button
         type="button"
         className="outline p-3 rounded-md hover:bg-slate-100"
-        onClick={onBox1}
+        onClick={clickHandler}
       >
         Create Summary
       </button>
