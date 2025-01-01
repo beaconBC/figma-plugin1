d86b4c366b43fbabb3ab4c33f95ca0bbc4bece7a
beaconBC <abhbtbb1@gmail.com>
2025-01-01 18:38:11 +0900

    feat: Add convertNewLine util funciton

--- ui-src/util.ts
+++ ui-src/util.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+export function convertNewLine(text: string) {
+  return text
+    .replace(/\<div\>\<br\>\<\/div\>/g, "\n")
+    .replace(/\<div\>/g, "\n")
+    .replace(/\<\/div\>/g, "");
+}
